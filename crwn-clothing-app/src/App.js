f7c6f698174c1f7b249c0ef581ad44eb8d90ab91
VengalreddyAkhila <akhila.it1801@gmail.com>
2022-12-14 09:30:50 +0530

    added:routers to component

--- crwn-clothing-app/src/App.js
+++ crwn-clothing-app/src/App.js
@@ -1,38 +1,13 @@
-import './categories.styles.scss'
-import CategoryItem from './components/category-item.component';
-import Directory from './directory/directory.component';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/home/home.component';
 const App = () => {
-
-  const categories =[
-  {
-    id:  1,
-    title:'Hats',
-    imageUrl:'crwn-clothing-app/Stetson_Tullamore_Distressed_Leather_Safari_Hat_HatsUnlimited.com_Hats_Unlimited_STW237_Brown_Stock_Image_1__06319.jpg'
-  },
-  {
-    id:  2,
-    title:'Jackets',
-    imageUrl:'C:/Users/91966/Desktop/React/React_Practiceprblm/crwn_clothing/crwn-clothing-app/th.jpg'
-  },
-  {
-    id:  3,
-    title:'Sneakers',
-    imageUrl:'crwn-clothing-app/th (1).jpg'
-  },
-  {
-    id:  4,
-    title:'Womens',
-    imageUrl:'crwn-clothing-app/th (2).jpg'
-  },
-  {
-    id:  5,
-    title:'Men',
-    imageUrl:'crwn-clothing-app/th (3).jpg'
-  },
-]
-  return (
-    <Directory categories={categories}/>
-  );
+return (
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+<Home/>
+</Routes>
+)
+ 
 }
 
 export default App;
---
+++ crwn-clothing-app/src/routes/home/home.component.js
@@ -0,0 +1,36 @@
+import Directory from "../../directory/directory.component";
+//import '../components/categories.styles.scss'
+const Home = () => {
+    const categories =[
+        {
+          id:  1,
+          title:'Hats',
+          imageUrl:'crwn-clothing-app/Stetson_Tullamore_Distressed_Leather_Safari_Hat_HatsUnlimited.com_Hats_Unlimited_STW237_Brown_Stock_Image_1__06319.jpg'
+        },
+        {
+          id:  2,
+          title:'Jackets',
+          imageUrl:'C:/Users/91966/Desktop/React/React_Practiceprblm/crwn_clothing/crwn-clothing-app/th.jpg'
+        },
+        {
+          id:  3,
+          title:'Sneakers',
+          imageUrl:'crwn-clothing-app/th (1).jpg'
+        },
+        {
+          id:  4,
+          title:'Womens',
+          imageUrl:'crwn-clothing-app/th (2).jpg'
+        },
+        {
+          id:  5,
+          title:'Men',
+          imageUrl:'crwn-clothing-app/th (3).jpg'
+        },
+      ]
+        return (
+          <Directory categories={categories}/>
+        );
+}
+
+export default Home
