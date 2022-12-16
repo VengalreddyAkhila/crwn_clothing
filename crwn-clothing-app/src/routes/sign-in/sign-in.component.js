ecdf1a16711d072b1bfdb3628b075ae4fcff3793
VengalreddyAkhila <akhila.it1801@gmail.com>
2022-12-16 15:08:57 +0530

    Added:sigin in authentication with google

--- crwn-clothing-app/src/routes/sign-in/sign-in.component.js
+++ crwn-clothing-app/src/routes/sign-in/sign-in.component.js
@@ -1,7 +1,28 @@
-const SignIn = () => {
+import { getRedirectResult } from "firebase/auth";
+import { useEffect } from "react";
+import { auth,
+    signInWithGooglePopup,
+    signInWithGoogleRedirect } from "../../utils/firebase/firebase.utils"
+import { createUserDocumentDromAuth } from "../../utils/firebase/firebase.utils";
+
+ const SignIn = () => {
+    useEffect(async () => {
+        const response = await getRedirectResult(auth);
+        return () => {
+        console.log(response)
+        }
+    },[])
+    const logGoogleUser = async () => {
+        const {user} = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentDromAuth(user)
+    }
+   
+   
 return(
     <div>
         <h1>Sign In Page</h1>
+        <button onClick = {logGoogleUser}>Sign In</button>
+        <button onClick = {signInWithGoogleRedirect}>Sign In with Redirect</button>
     </div>
 )
 }
--- crwn-clothing-app/src/utils/firebase/firebase.utils.js
+++ crwn-clothing-app/src/utils/firebase/firebase.utils.js
@@ -1,4 +1,11 @@
 import { initializeApp } from "firebase/app";
+import {getAuth,signInWithRedirect,signInWithPopup,GoogleAuthProvider} from 'firebase/auth'
+
+import {getFirestore,
+doc,
+getDoc,
+setDoc
+} from 'firebase/firestore'
 
 
 // Your web app's Firebase configuration
@@ -12,4 +19,36 @@ const firebaseConfig = {
   };
   
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  const firebaseApp = initializeApp(firebaseConfig);
+  const googleProvider = new GoogleAuthProvider();
+  googleProvider.setCustomParameters({
+    prompt:"select_account"
+  })
+
+  export const auth = getAuth();
+  export const signInWithGooglePopup = () => signInWithPopup(auth,googleProvider)
+  export const signInWithGoogleRedirect = () => signInWithRedirect(auth,googleProvider)
+
+  export  const db = getFirestore();
+  export const createUserDocumentDromAuth = async (userAuth) => {
+      const userDocRef = doc(db, 'users', userAuth.uid)
+      console.log(userDocRef)
+
+      const userSnapshot = await getDoc(userDocRef)
+      console.log(userSnapshot)
+      console.log(userSnapshot.exists())
+
+      if(!userSnapshot.exists()){
+        const {displayName,email} = userAuth;
+        const createdAt = new Date();
+        try{
+          await setDoc(userDocRef,{
+            displayName,
+            email,createdAt
+          });
+        }catch(error){
+           console.log('error creating the user' , error.message)
+        }
+      }
+  }
+  
